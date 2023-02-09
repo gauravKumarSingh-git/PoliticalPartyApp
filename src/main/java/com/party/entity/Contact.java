0e1e97e6ae3e79ed46571fc588f600f3fdc03ddc
Amar Khedkar <smartynew0@gmail.com>
2023-02-09 14:14:13 +0530

    Update Contact.java

--- src/main/java/com/party/entity/Contact.java
+++ src/main/java/com/party/entity/Contact.java
@@ -1,5 +1,35 @@
 package com.party.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class Contact {
-    
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private String name;
+    private String description;
+
+    public Contact(String name , String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+		this.name = name;
+	}
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+		this.description = description;
+	}
+
 }
